983a732ac6efef49aabcec9e3af1f2e39d89d3be
Liane Hampe <liaham@xmera.de>
2024-07-25 15:24:40 +0200

    Changes development environment
    Adds our development hostname to the configuration
    of allowed hosts.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -59,4 +59,7 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  # [xmera]
+  config.hosts << devmac
 end
