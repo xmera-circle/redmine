a9cef53b2503626624142b222cc03097f30316bb
Liane Hampe <liaham@xmera.de>
2024-07-25 15:37:28 +0200

    Further fixing development host

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -60,6 +60,6 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  # [xmera]
-  config.hosts << 'devmac:3000'
+  ## always required with Rails 6
+  config.hosts << "devmac" # [xmera]
 end
